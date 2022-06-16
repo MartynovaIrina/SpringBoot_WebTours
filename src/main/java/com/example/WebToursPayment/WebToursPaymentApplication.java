88a15a7ab6a76775bae0bd00f19663b612fc1468
Irina <irinamartynova92@gmail.com>
2022-06-16 14:16:15 +0300

    test3

--- src/main/java/com/example/WebToursPayment/WebToursPaymentApplication.java
+++ src/main/java/com/example/WebToursPayment/WebToursPaymentApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebToursPaymentApplication {
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(WebToursPaymentApplication.class, args);
 	}
